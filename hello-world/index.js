b37e03e16007eaf7dc16c6de6fcbf3927bca093e
Alberto Mcwhirter-Javier <79724781+xmen12cc@users.noreply.github.com>
2024-12-21 18:46:13 +0000

    Completed

---
+++ hello-world/index.js
@@ -0,0 +1,13 @@
+console.log('Hello World, from a script file.');
+
+import fetch from 'node-fetch';
+
+console.log(`start`);
+try {
+  const res = await fetch('https://github.com/MicrosoftDocs/node-essentials');
+
+  console.log('statusCode:', res.status);
+} catch (error) {
+  console.log(`error: ${error}`);
+}
+console.log(`end`);
