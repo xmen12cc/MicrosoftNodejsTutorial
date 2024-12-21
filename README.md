e6e66694d325f58c4b9e705e4928e3db94f71f64
Alberto Mcwhirter-Javier <79724781+xmen12cc@users.noreply.github.com>
2024-12-21 20:08:17 +0000

    Completed 2nd module

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Completed the Microsoft Education introduction to Node.Js
 
 <a href="https://learn.microsoft.com/en-us/training/modules/intro-to-nodejs/">Found here</a>
+
+Completed the Microsoft Education "Create a new Node.js project and work with dependencies" 
+
+<a href="https://learn.microsoft.com/en-us/training/modules/create-nodejs-project-dependencies/">Found here</a>
+
+Currently taking the Microsoft Education "Interactively debug Node.js apps with the built-in and Visual Studio Code debuggers" 
+
+<a href="https://learn.microsoft.com/en-us/training/modules/debug-nodejs/">Found here</a>
--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,23 @@
-console.log('Hello World, from a script file.');
+console.log('Hello from the index.js file');
+
+const fetch = require('node-fetch')
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
+
+async function run() {
+  const response = await fetch("https://dev.to/api/articles?state=rising");
+  const json = await response.json();
+  const sorted = _.sortBy(json, ["public_reactions_count"], ['desc']);
+  const top3 = _.take(sorted, 3);
+
+  const filePrefix = new Date().toISOString().split('T')[0];
+  fs.writeFileSync(path.join(__dirname, `${filePrefix}-feed.json`), JSON.stringify(top3, null, 2));
+}
+
+run();
+
+/*console.log('Hello World, from a script file.');
 
 import fetch from 'node-fetch';
 
@@ -11,3 +30,4 @@ try {
   console.log(`error: ${error}`);
 }
 console.log(`end`);
+*/
